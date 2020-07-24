599fda753faf6dfebc597bd32b73d596571237a3
Laercio <laerciodiniz@gmail.com>
2020-07-23 23:54:04 -0300

    chore: Configuring structure, statusbar, navigation and Fonts

--- appgobarber/src/App.tsx
+++ appgobarber/src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import 'react-native-gesture-handler';
 
-const App: React.FC = () => {
-  return (
-    <>
-      <View>
-        <Text>Hello World</Text>
-      </View>
-    </>
-  );
-};
+import React from 'react';
+import { View, Text, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
 export default App;
