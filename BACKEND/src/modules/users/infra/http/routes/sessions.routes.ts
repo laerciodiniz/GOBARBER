c65d4c169498d5dff8b5c53706a7e680d6908b00
Laercio <laerciodiniz@gmail.com>
2020-08-19 00:11:21 -0300

    chore: SOLID concepts, Liskov...Inversion...Injection

--- BACKEND/src/modules/users/infra/http/routes/sessions.routes.ts
+++ BACKEND/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,19 +1,10 @@
 import { Router } from 'express';
 
-import AuthenticateUserService from '@modules/users/services/AuthenticateUserService';
+import SessionsController from '../controllers/SessionsController';
 
 const SessionsRouter = Router();
+const sessionController = new SessionsController();
 
-SessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = new AuthenticateUserService();
-
-  const { user, token } = await authenticateUser.execute({ email, password });
-
-  delete user.password;
-
-  return response.json({ user, token });
-});
+SessionsRouter.post('/', sessionController.create);
 
 export default SessionsRouter;
