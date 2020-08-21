d655893b0560e974f59c6002bfbbf545bedba9cc
Laercio <laerciodiniz@gmail.com>
2020-08-20 23:58:21 -0300

    chore: implemented Tests and TDD

---
+++ BACKEND/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,41 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123321',
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(); // Date(2020, 9, 20, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123321',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
