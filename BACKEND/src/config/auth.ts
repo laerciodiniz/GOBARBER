d0d8b97851b92b89bdbe7802d5a3857217b392bd
Laercio <laerciodiniz@gmail.com>
2020-09-05 23:28:15 -0300

    chore: data validation, created environment variables and applying the class transformer

--- BACKEND/src/config/auth.ts
+++ BACKEND/src/config/auth.ts
@@ -1,6 +1,6 @@
 export default {
   jwt: {
-    secret: 'e82f21a5802c3285975cf16f88c3c811',
+    secret: process.env.APP_SECRET,
     expiresIn: '1d',
   },
 };
--- BACKEND/src/modules/users/infra/http/routes/sessions.routes.ts
+++ BACKEND/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import SessionsController from '../controllers/SessionsController';
 
 const SessionsRouter = Router();
 const sessionController = new SessionsController();
 
-SessionsRouter.post('/', sessionController.create);
+SessionsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  sessionController.create,
+);
 
 export default SessionsRouter;
